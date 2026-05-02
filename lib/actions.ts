13c4fda20927aabe474125cd3d80eb2e757a8321
Aaron Mark Sayson <saysonaaronmark@gmail.com>
2026-05-02 20:03:54 +0800

    add tanstack table; code refactors

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { redirect, RedirectType } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export async function analyzeGames(formData: FormData) {
   const username = formData.get("username");
 
-  redirect(`./${username}/accuracy`, RedirectType.push);
+  redirect(`/analyze/${username}/accuracy`);
 }
